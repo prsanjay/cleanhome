48732d379b56382a0781d5349bf0625bb51ca2b0
prsanjay <lnvg530@gmail.com>
2017-05-07 10:24:53 +0530

    Add admin links in partial file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-   cities = City.create([{ name: 'Ahmedabad' }, { name: 'Surat' },{ name: 'Vadodara' },{ name: 'Rajkot' },{ name: 'Bhavnagar' },{ name: 'Jamnagar' },{ name: 'Morbi' },{ name: 'Modasa' },{ name: 'Vapi' },{ name: 'Valsad' }])
+cities = City.create([{ name: 'Ahmedabad' }, { name: 'Surat' },{ name: 'Vadodara' },{ name: 'Rajkot' },{ name: 'Bhavnagar' },{ name: 'Jamnagar' },{ name: 'Morbi' },{ name: 'Modasa' },{ name: 'Vapi' },{ name: 'Valsad' }])
 
-   admin = Admin.create(email: 'admin@admin.com',password: "12345")
+admin = Admin.create(email: 'admin@admin.com',password: "12345")
