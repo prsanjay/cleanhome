62bad29b1177024f30306b0899ba7a56dba3b1d1
prsanjay <lnvg530@gmail.com>
2017-05-07 11:09:08 +0530

    Add Model validation for quality score and email

--- app/models/cleaner.rb
+++ app/models/cleaner.rb
@@ -3,7 +3,13 @@ class Cleaner < ActiveRecord::Base
   has_many :city_cleaners
   has_many :cities,through: :city_cleaners
 
-  validates :first_name,:last_name,:email_id, presence: true
+  validates :first_name,:last_name,:email_id,:quality_score, presence: true
+
+  validates :quality_score, inclusion: { in: 0.0..5.0,
+    message: "should be between 0.0 and 5.0" }
+
+  validates_format_of :email_id, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
 
   def add_cities(cities)
     CityCleaner.where(cleaner_id: self.id).destroy_all
